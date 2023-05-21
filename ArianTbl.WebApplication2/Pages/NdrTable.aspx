7cc2faca5a66fd2cbe4419de6a8b5d7c2cba039b
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-21 08:57:48 +0430

    Add project files.

---
+++ ArianTbl.WebApplication2/Pages/NdrTable.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SimpleForm.master" AutoEventWireup="true" CodeBehind="NdrTable.aspx.cs" Inherits="PayamHannan.Arian.Management.Web.NDR.NdrTable" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="SimpleFormPlace" runat="server">
+    <script src="../Scripts/NDR/NdrTable.js"></script>
+    <div class="grid" id="mainPage">
+
+
+    </div>
+
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ModalsContent" runat="server">
+
+    
+
+</asp:Content>
---
+++ ArianTbl.WebApplication2/Pages/NdrTable.ts
@@ -0,0 +1,25 @@
+﻿class NdrTable extends Card {
+    docs: Grid<any> = null;
+    table: Table;
+
+    constructor($mainPage:JQuery) {
+        super(' ');
+
+        this.$card.appendTo($mainPage);
+
+        var $row = $('<div class="row" />').appendTo(this.$body);
+        var $col4 = $('<div class="col col-4" />').append('&nbsp;').appendTo($row);
+        var $col8 = $('<div class="col col-8 text-left" />').appendTo($row);
+        this.table = new Table(["col1", "col2"]);
+        this.table.addRow(["col1", "col2"]);
+        this.table.$table.appendTo(this.$card);
+        
+    }
+
+    
+}
+
+$(function () {
+    var tbl = new NdrTable($("#mainPage"));
+    
+})
